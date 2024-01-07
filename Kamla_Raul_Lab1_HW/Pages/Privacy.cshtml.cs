378c984cf08c9b0809cb2b3c68811f7246bcb70b
nadiadobra <nadiadobra@RO-2WGZ5M3>
2024-01-07 16:38:52 +0200

    Add project files.

---
+++ Kamla_Raul_Lab1_HW/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kamla_Raul_Lab1_HW.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
